1ecc244f55d6e9e25382fd731b06ea7db5f57967
Ravi-Ranjan-90 <146408420+Ravi-Ranjan-90@users.noreply.github.com>
2024-08-31 12:16:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Word Ladder II/word-ladder-ii.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> s(wordList.begin(),wordList.end());
+        queue<vector<string>>q;
+        q.push({beginWord});
+        int level=0;
+        vector<string> usedOnLevel;
+        usedOnLevel.push_back(beginWord);
+         vector<vector<string>> ans;
+        if (s.find(endWord) == s.end()) return {};
+         while(!q.empty()){
+             vector<string> vec=q.front();
+             string word=vec.back();
+             q.pop();
+             if(level<vec.size()){
+                for(auto it:usedOnLevel){
+                    s.erase(it);
+                }
+                level++;
+             }
+             if(word==endWord){
+                if(ans.size()==0){
+                    ans.push_back(vec);
+                }
+                else if(ans[0].size()==vec.size()){
+                     ans.push_back(vec);
+                }
+             }
+             for(int i=0;i<word.size();i++){
+                char original=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(s.count(word)>0){
+                        vec.push_back(word);
+                        usedOnLevel.push_back(word);
+                        q.push(vec);
+                        vec.pop_back();
+                    }
+                }
+                word[i]=original;
+             }
+         }
+         return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+bool comp(vector<string> a, vector<string> b)
+{
+    string x = "", y = "";
+    for(string i: a)
+        x += i;
+    for(string i: b)
+        y += i;
+    
+    return x<y;
+}
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<string>wordList(n);
+		for(int i = 0; i < n; i++)cin >> wordList[i];
+		string startWord, targetWord;
+		cin >> startWord >> targetWord;
+		Solution obj;
+		vector<vector<string>> ans = obj.findSequences(startWord, targetWord, wordList);
+		if(ans.size()==0)
+		    cout<<-1<<endl;
+		else
+		{
+		    sort(ans.begin(), ans.end(), comp);
+            for(int i=0; i<ans.size(); i++)
+            {
+                for(int j=0; j<ans[i].size(); j++)
+                {
+                    cout<<ans[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
